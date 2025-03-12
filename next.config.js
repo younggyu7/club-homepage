f6eed45b37dc9f1a391a81f18c962c9844a24a91
younggyu7 <pyochang1@yonsei.ac.kr>
2025-03-12 22:38:35 +0900

    FIx: dfdfdf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone", // ✅ Vercel에서 동적 페이지를 제대로 인식하게 함
-  trailingSlash: true,   // ✅ URL 끝에 슬래시 추가하여 라우팅 문제 해결 가능
+  output: "standalone",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: {
+              plugins: [
+                require("postcss-import"),
+                require("tailwindcss"),
+                require("autoprefixer"),
+              ],
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css"
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
 
