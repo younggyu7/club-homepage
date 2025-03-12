3209d8c0fe40d6bf8fa9c7fc523265adb17b4b2d
younggyu7 <pyochang1@yonsei.ac.kr>
2025-03-12 23:51:41 +0900

    Fix: Correct PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "@tailwindcss/postcss": {},
-    "autoprefixer": {},
-  },
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
 
