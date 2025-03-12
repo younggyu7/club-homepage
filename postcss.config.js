809fe7256a5bb36fe442a72bef135a13e57307fd
younggyu7 <pyochang1@yonsei.ac.kr>
2025-03-12 23:01:55 +0900

    Fix: SyntaxError in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    'postcss-import': {},
-    'tailwindcss': {},
-    'autoprefixer': {}
-  ]
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
 
